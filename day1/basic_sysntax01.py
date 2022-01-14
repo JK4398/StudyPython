7a6e1c671ee4023368b62ecc654110f3ff5b037a
jk4398 <woojin4398@naver.com>
2022-01-14 17:09:33 +0900

    파이썬 1일차 진행

---
+++ day1/basic_sysntax01.py
@@ -0,0 +1,42 @@
+# 주석입니다
+# 만약 따옴표를 같이 쓰고 싶으면 위와 같이 사용할 것.
+print('"Hello,World"')
+print("'Hello,World'")
+
+# 숫자 연산 출력 print () 
+print(3+4)
+
+
+# 변수사용
+a = '안녕하세요.'
+b = '반갑습니다!!'
+print(a)
+print(b)
+print(a,b,'정말?')
+
+
+# 수학연산
+# '/'는 정확한 나누기, '//'는 정수로만, '%'는 남은 값
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/3)
+print(5//3)
+print(5%3)
+print(2**10)
+
+print(int(5/3))
+print(int(10))
+# print(int('ten'))  #형변환안됨
+
+
+print(float(5))
+
+# type 확인
+print(type(10))
+print(type(10.0))
+
+#괄호 (연산자 우선순위)
+print((2+3)*5)
+print(2+3*5)
+
